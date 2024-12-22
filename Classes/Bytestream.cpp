8e4004714da8688e98c2e3230f41bdab310c2feb
Natesworks <contact@natesworks.com>
2024-12-22 14:34:29 +0100

    Start working on bytestream

---
+++ Classes/Bytestream.cpp
@@ -0,0 +1,64 @@
+#include "Bytestream.h"
+#include "ChecksumEncoder.h"
+
+int Bytestream::readInt()
+{
+    bitoffset = 0;
+    int result = (messagePayload[offset] << 24);
+    result += (messagePayload[offset + 1] << 16);
+    result += (messagePayload[offset + 2] << 8);
+    result += (messagePayload[offset + 3]);
+    offset += 4;
+    return result;
+}
+
+int Bytestream::readInt8()
+{
+    bitoffset = 0;
+    return messagePayload[offset++];
+}
+
+int Bytestream::readInt16()
+{
+    bitoffset = 0;
+    int result = (messagePayload[offset] << 8);
+    result += (messagePayload[offset + 1]);
+    offset += 2;
+    return result;
+}
+
+int Bytestream::readInt24()
+{
+    bitoffset = 0;
+    int result = (messagePayload[offset] << 16);
+    result += (messagePayload[offset + 1] << 8);
+    result += (messagePayload[offset + 2]);
+    offset += 3;
+    return result;
+}
+
+int Bytestream::readIntLittleEndian()
+{
+    bitoffset = 0;
+    int result = (messagePayload[offset]);
+    result += (messagePayload[offset + 1] << 8);
+    result += (messagePayload[offset + 2] << 16);
+    result += (messagePayload[offset + 3] << 24);
+    offset += 4;
+    return result;
+}
+
+void Bytestream::writeIntToByteArray(int value)
+{
+    messagePayload.push_back((value >> 24) & 0xFF);
+    messagePayload.push_back((value >> 16) & 0xFF);
+    messagePayload.push_back((value >> 8) & 0xFF);
+    messagePayload.push_back(value & 0xFF);
+}
+
+void Bytestream::writeInt(int value)
+{
+    ChecksumEncoder checksumEncoder;
+    checksumEncoder.writeInt(value);
+    writeIntToByteArray(value);
+}
---
+++ Classes/Bytestream.h
@@ -0,0 +1,27 @@
+#ifndef BYTESTREAM_H
+#define BYTESTREAM_H
+
+#include <vector>
+#include <cstdint>
+#include <stddef.h>
+#include "LogicLong.h"
+
+class Bytestream
+{
+public:
+    std::vector<uint8_t> messagePayload;
+    size_t bitoffset;
+    size_t offset;
+    size_t length;
+    int readInt();
+    int readInt8();
+    int readInt16();
+    int readInt24();
+    int readIntLittleEndian(); // Little Endian is pretty much the same as Big Endian, but the bytes are reversed. 
+    int readLong(LogicLong *logicLong);
+    void readLongLong();
+    void writeInt(int value);
+    void writeIntToByteArray(int value);
+};
+
+#endif
